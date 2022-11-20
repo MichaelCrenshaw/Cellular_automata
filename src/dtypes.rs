73e492bea3bf26c91d1b30c9ccf913e4cab3122d
MichaelCrenshaw <57016022+MichaelCrenshaw@users.noreply.github.com>
2022-11-19 19:52:15 -0700

    init compute

--- src/dtypes.rs
+++ src/dtypes.rs
@@ -1,18 +1,19 @@
 pub(crate) mod dtypes {
+    use std::fmt::{Debug, Formatter};
     use std::ops::Index;
 
-    struct BitBoard2D {
+    struct BitBoard2D<'a> {
         data: Vec<u8>,
-        dims: [u32; 2]
+        dims: &'a [u32]
     }
 
-    impl BitBoard2D {
-        pub fn new(dims: [u32; 2]) -> Self {
-            let board = (0..(dims[0] * dims[1])).into_iter().map(|_| 0u8).collect::<Vec<u8>>();
-            BitBoard2D {
+    impl BitBoard2D<'_> {
+        pub fn new(dims: &[u32]) -> Box<BitBoard2D<'_>> {
+            let board = (0..(dims[0] * dims[1])).into_iter().map(|_| 0b_0000_0000).collect::<Vec<u8>>();
+            Box::new(BitBoard2D {
                 data: board,
-                dims: dims,
-            }
+                dims,
+            })
         }
 
         pub fn len(&self) -> u32 {
@@ -24,30 +25,27 @@ pub(crate) mod dtypes {
         }
 
         pub fn get_byte(&self, address: &[u32; 2]) -> Option<&u8> {
-            let width = self.dims[0];
-            let x_addr = address[0];
-            let y_addr = address[1];
+            let index = address[0] + (address[1] * self.dims[0]);
+            println!("{}", index);
 
-            match self.data.get::<usize>((width * y_addr + x_addr) as usize) {
+            match self.data.get::<usize>(index as usize) {
                 None => {None}
                 Some(x) => {Some(x as &u8)}
             }
         }
 
         pub fn get(&self, address: &[u32; 2]) -> Option<bool> {
-            let u8_group: &u8 = self.get_byte(
-                &[(address[0] as f64 / 8.0).floor() as u32,
-                    (address[0] as f64 / 8.0).floor() as u32])?;
+            let u8_group: &u8 = self.get_byte(&[(address[0] / 8) as u32, (address[1] / 8) as u32])?;
 
-            let rem = (address[0] + address[1]) % 8;
+            let rem = (address[0] + (address[1] * self.dims[0])) % 8;
 
-            let mask: u8 = 0b_1000_0000 >> rem;
+            let mask: u8 = 0b_0000_0001 << rem;
 
             Some((mask & u8_group) != 0b_0000_0000)
         }
 
         pub fn set_byte(&mut self, address: &[u32; 2], value: u8) -> Result<(), &str> {
-            let index = address[0] + address[1] * self.dims[0];
+            let index = address[0] + (address[1] * self.dims[0]);
             if index > self.len() {
                 return Err("Out of index range");
             }
@@ -60,14 +58,14 @@ pub(crate) mod dtypes {
         pub fn set(&mut self, address: &[u32; 2], value: bool) -> Result<(), &str> {
             // validity of address is only checked once, in set_byte
             // as such this function assumes a valid index until the very end
-            let byte_address = &[(address[0] as f64 / 8.0).floor() as u32, (address[0] as f64 / 8.0).floor() as u32];
-            let mut byte = match self.get_byte(byte_address) {
+            let byte_index = &[(address[0] / 8) as u32, (address[1] / 8) as u32];
+
+            let mut byte = match self.get_byte(byte_index) {
                 None => { return Err("Address is out of index") }
                 Some(b) => {b.to_owned()}
             };
 
-
-            let rem = (address[0] + address[1]) % 8;
+            let rem = (address[0] + (address[1] * self.dims[0])) % 8;
 
             if value {
                 byte &= !(0b_1000_0000 >> rem)
@@ -75,11 +73,31 @@ pub(crate) mod dtypes {
                 byte |= 0b_1000_0000 >> rem
             }
 
-            self.set_byte(byte_address, byte)?;
+            self.set_byte(byte_index, byte)?;
 
             Ok(())
         }
+    }
 
+    impl Debug for BitBoard2D<'_> {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            let mut out = String::new();
+            // print header
+            out += &*format!("{:^8}", '+');
+            for x in 0..(self.dims[0]*8) {out += &*format!("{:^8}", x)}
+
+            for y in 0..(self.dims[0]) as u32 {
+                out += "\r\n";
+                out += &*format!("{:^8}", y);
+                for x in 0..(self.dims[1]*8) as u32 {
+                    println!("{}, {}", x, y);
+                    out += &*format!("{:^8}", self.get(&[x, y]).unwrap())
+                }
+            }
+
+            f.write_str(&*out);
+            Ok(())
+        }
     }
 
     #[cfg(test)]
@@ -88,7 +106,7 @@ pub(crate) mod dtypes {
 
         #[test]
         fn test_board_inst() {
-            let board = BitBoard2D::new([6, 10]);
+            let board = BitBoard2D::new(&[6, 10]);
 
             assert_eq!(board.data.len(), 60);
             assert_eq!(board.dims, [6, 10]);
@@ -96,7 +114,7 @@ pub(crate) mod dtypes {
 
         #[test]
         fn test_board_set_read() {
-            let mut board = BitBoard2D::new([10, 10]);
+            let mut board = BitBoard2D::new(&[10, 10]);
 
             let mut cases: Vec<([u32; 2], u8)> = vec![
                 ([0, 0], 0),
@@ -113,6 +131,9 @@ pub(crate) mod dtypes {
                     Ok(_) => {}
                 }
             }
+
+            println!("{}", format!("{:?}", board));
+
             // remove overwritten byte
             cases.remove(1);
             for (x, y) in &cases {
@@ -123,9 +144,36 @@ pub(crate) mod dtypes {
             }
         }
 
-        #[test]
+        // #[test]
         fn test_board_set_read_bitwise() {
+            let mut board = BitBoard2D::new(&[10, 10]);
 
+            let mut cases: Vec<([u32; 2], bool)> = vec![
+                ([0, 0], false),
+                ([79, 79], false),
+                ([79, 0], false),
+                ([0, 79], false),
+                // ensure bits can be overwritten
+                ([0, 0], true),
+            ];
+
+            for (x, y) in &cases {
+                match board.set(x, *y) {
+                    Err(_) => {panic!("{}", format!("Could not set bit at {:?} to {}", x, y))}
+                    Ok(_) => {}
+                }
+            }
+
+            println!("{}", format!("{:?}", board));
+
+            // remove overwritten byte
+            cases.remove(0);
+            for (x, y) in &cases {
+                match board.get(x) {
+                    None => {panic!("{}" , format!("Attempted to get from out of index at {:?}", x))}
+                    Some(b) => { if y != &b {panic!("{}", format!("Bit at {:?} not equal to {}", x, y))} }
+                }
+            }
         }
     }
 }
--- src/trivial.rs
+++ src/trivial.rs
@@ -1,36 +1,173 @@
 pub(crate) mod trivial {
     extern crate ocl;
-    use ocl::ProQue;
+
+    use std::ops::Index;
+    use ocl::{flags, Platform, Device, Context, Queue, Program, Buffer, Kernel};
 
     pub(crate) fn trivial_fn() -> ocl::Result<()> {
         let src = r#"
-            __kernel void add(__global float* buffer, float scalar) {
-                buffer[get_global_id(0)] += scalar;
+            __kernel void add(
+                __global uint* in_buffer,
+                __global uint* out_buffer,
+                __global int* stencil_buffer,
+                uint stencil_size,
+                int array_size)
+            {
+                int neighbors = 0;
+
+                for (int i = 0; i < stencil_size; i++) {
+                    int index = stencil_buffer[i] + get_global_id(0);
+                    if (index >= 0 && index <= array_size) {
+                        neighbors += in_buffer[index];
+                    }
+                }
+
+                out_buffer[get_global_id(0)] = neighbors;
+                return;
+
+                if (neighbors == 2) {
+                    out_buffer[get_global_id(0)] = in_buffer[get_global_id(0)];
+                    return;
+                }
+
+                if (neighbors == 3) {
+                    out_buffer[get_global_id(0)] = 1;
+                    return;
+                }
+
+                out_buffer[get_global_id(0)] = 0;
             }"#;
 
-        let pro_que = ProQue::builder()
+        let dimensions = [10u32, 10u32];
+
+        let stencil: [[i32; 2]; 8] = [
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [1, -1],
+            [1, 0],
+            [1, 1],
+            [0, -1],
+            [0, 1],
+        ];
+
+        let platform = Platform::default();
+        let device = Device::first(platform)?;
+        let context = Context::builder()
+            .platform(platform)
+            .devices(device.clone())
+            .build()?;
+
+        let program = Program::builder()
+            .devices(device)
             .src(src)
-            .dims(1 << 20)
+            .build(&context)?;
+
+        let queue = Queue::new(&context, device, None)?;
+        let dims = 360;
+
+        let stencil_buffer = Buffer::<i32>::builder()
+            .queue(queue.clone())
+            .flags(flags::MEM_READ_ONLY)
+            .len(stencil.len())
+            .fill_val(0)
             .build()?;
 
-        let mut buffer = pro_que.create_buffer::<f32>()?;
+        let mut stencil_vec: Vec<i32> = Vec::with_capacity(stencil.len());
 
-        let kernel = pro_que.kernel_builder("add")
-            .arg(&buffer)
-            .arg(10.0f32)
+        for s in stencil.iter() {
+            let mut index: i32 = s[0];
+            for (d, z) in dimensions.iter().enumerate() {
+                if d == 0 {
+                    continue;
+                }
+
+                let mut offset = 1i32;
+                for x in dimensions[0..d].into_iter() { offset *= *x as i32 }
+                println!("{} + {:?} = {}", s[0], &dimensions[0..d], offset * s[d]);
+                index += s[d] * offset;
+            }
+            stencil_vec.push(index);
+        }
+
+        unsafe {
+            stencil_buffer.write(&stencil_vec).enq().unwrap();
+        }
+
+        let mut in_buffer = Buffer::<u32>::builder()
+            .queue(queue.clone())
+            .flags(flags::MEM_READ_ONLY)
+            .len(dims)
             .build()?;
 
-        let kernel2 = pro_que.kernel_builder("add")
-            .arg(&buffer)
-            .arg(355.0f32)
+        let mut array_vec = Vec::with_capacity(in_buffer.len());
+
+        for index in 0..in_buffer.len() {
+            if index % 3 == 0 {
+                array_vec.push(1);
+            } else {
+                array_vec.push(0);
+            }
+        }
+
+        unsafe {
+            in_buffer.write(&array_vec).enq().unwrap();
+        }
+
+        println!("{:?}", in_buffer);
+
+        let out_buffer = Buffer::<u32>::builder()
+            .queue(queue.clone())
+            .flags(flags::MEM_WRITE_ONLY)
+            .len(dims)
+            .fill_val(0)
             .build()?;
 
-        unsafe { kernel.enq()?; kernel2.enq()?; }
+        println!("Output buffer len is: {:?}", out_buffer.len());
 
-        let mut vec = vec![0.0f32; buffer.len()];
-        buffer.read(&mut vec).enq()?;
+        let kernel = Kernel::builder()
+            .program(&program)
+            .name("add")
+            .queue(queue.clone())
+            .global_work_size(dims)
+            .arg(&in_buffer)
+            .arg(&out_buffer)
+            .arg(&stencil_buffer)
+            .arg(&(stencil.len() as u32))
+            .arg(&dimensions[0] * dimensions[1])
+            .build()?;
 
-        println!("The value at index [{}] is now '{}'!", 200007, vec[200007]);
+        unsafe {
+            kernel.cmd()
+                .queue(&queue)
+                .global_work_offset(kernel.default_global_work_offset())
+                .global_work_size(dims)
+                .local_work_size(kernel.default_local_work_size())
+                .enq()?;
+        }
+
+        let mut vec = vec![0; dims];
+        in_buffer.read(&mut vec).enq()?;
+
+        for x in 0..(dimensions[0] * dimensions[1]) {
+            if x.rem_euclid(10) == 0 {
+                println!("\n");
+            }
+            print!("{:^8?}", vec[x as usize]);
+        }
+        println!("\n");
+
+        let mut vec = vec![0; dims];
+        out_buffer.read(&mut vec).enq()?;
+
+        for x in 0..(dimensions[0] * dimensions[1]) {
+            if x.rem_euclid(10) == 0 {
+                println!("\n");
+            }
+            print!("{:^8?}", vec[x as usize]);
+        }
+        println!("\n");
+
         Ok(())
     }
 }
